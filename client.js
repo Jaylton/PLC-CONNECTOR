66706f543c71e7e0ea5042d3523253578f439fcd
jaylton <jayltonokra@gmail.com>
2023-01-08 23:02:21 -0300

    funcionando com o simulador

--- client.js
+++ client.js
@@ -17,35 +17,37 @@ socket.on('connect', function () {
 
 const { db } = require("./fb");
 
-async function get(){
+async function get(){ //get data from firebase
     const doc = db.collection('tags');
-    const observer = doc.onSnapshot(docSnapshot => {
+    const observer = doc.onSnapshot(docSnapshot => {//getting realtime changes
         console.log(`Received doc snapshot:`);
         const tags = docSnapshot.docs.map((doc) => ({
             id: doc.id,
             ...doc.data(),
         }));
-        // console.log('tags :>> ', tags);
         tags.forEach(element => {
             console.log('element :>> ', element);
-            if(element.fc === 16){//register
-                // server.holding.writeFloatBE(element.value, 0)
-
-            }else if(element.fc === 15){//coil
+            if(element.fc === 16){// write register
+                client.writeSingleRegister(element.address, element.value)
+                .then(function (resp) {
+                    console.log('write Register :>> ', element.address, element.value);
+                    console.log('write Register value :>> ', resp.response.body.value);
+                }).catch(function () {
+                    console.error(arguments)
+                });
+            }else if(element.fc === 15){//write coil
                 client.writeSingleCoil(element.address, element.value === 1)
-                    .then(function (resp) {
-                        console.log('write element.address :>> ', element.address);
-                    }).catch(function () {
-                        console.error(arguments)
-                    })
-
+                .then(function (resp) {
+                    console.log('write coil :>> ', element.address);
+                }).catch(function () {
+                    console.error(arguments)
+                });
             }
         });
     }, err => {
       console.log(`Encountered error: ${err}`);
     });
 }
-
 
 socket.on('error', console.error)
 socket.connect(options)
--- server.js
+++ server.js
@@ -1,39 +1,25 @@
 'use strict'
 
-//como escrever no modbus?
-
 const net = require('net')
 const modbus = require('jsmodbus')
 const netServer = new net.Server()
 const holding = Buffer.alloc(10000)
 const server = new modbus.server.TCP(netServer, {
   holding: holding,
-//   coils: Buffer.from([0x55, 0x55, 0x55])
 });
 require('./client.js');
 
 const { db } = require("./fb");
 
 async function post(value, address, fc){
-    await db.collection("tags").doc(fc+'-'+address).set({
-        name: 'test post',
+    await db.collection("tags").doc(fc+'-'+address).set({ //save tags
         value,
         fc,
         address
       });
 }
 server.on('connection', function (client) {
-    // console.log('client :>> ', client);
     console.log('New Connection')
-    // get();
-    // post();
-    // server.coils.writeUInt16BE(0x0001, 0)
-    // server.coils.writeUInt16BE(0x0001, 2)
-    // server.coils.writeUInt16BE(0x0001, 4)
-    // server.coils.writeUInt16BE(0x0001, 6)
-
-    // server.discrete.writeUInt16BE(0x5678, 0)
-    // server.discrete.writeUInt16BE(0x5678, 2)
 })
 
 server.on('readCoils', function (request, response, send) {
@@ -71,10 +57,6 @@ server.on('postWriteMultipleCoils', function (value) {
     }
 })
 
-/* server.on('writeMultipleRegisters', function (value) {
-  console.log('Write multiple registers - Existing: ', value)
-}) */
-
 server.on('postWriteMultipleRegisters', function (value) {
     try {
         console.log();
@@ -83,7 +65,7 @@ server.on('postWriteMultipleRegisters', function (value) {
             const fc = value._body._fc;
             console.log('fc :>> ', fc);
             console.log('address :>> ', address);
-            const val = value._body._values.readFloatBE()
+            const val = value._body._values.readInt16BE()
             console.log('val :>> ', val);
             post(val, address, fc);
         }
@@ -95,8 +77,8 @@ server.on('postWriteMultipleRegisters', function (value) {
 // server.holding.writeUInt16BE(0x0000, 0)
 // server.holding.writeUInt16BE(0x0000, 2)
 
-server.input.writeUInt16BE(0xff00, 0)
-server.input.writeUInt16BE(0xff00, 2)
+// server.input.writeUInt16BE(0xff00, 0)
+// server.input.writeUInt16BE(0xff00, 2)
 
 console.log('port: '+10502)
 netServer.listen(10502)

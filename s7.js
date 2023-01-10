f63f23e90d3c82cb4637c56f77c5ffa3b6b9bad0
Jaylton Alencar <jayltonokra15@gmail.com>
2023-01-10 17:43:10 -0300

    siemens funcionando escrita e leitura

--- s7.js
+++ s7.js
@@ -1,35 +1,74 @@
 var nodes7 = require('nodes7'); // This is the package name, if the repository is cloned you may need to require 'nodeS7' with uppercase S
+
+const { db } = require("./fb");
+
 var conn = new nodes7;
 var doneReading = false;
 var doneWriting = false;
+var firstLoad = false;
 var tags = {};
 
-var variables = {
-      TEST2: 'M0.0',        // Bit at M32.2
-      TEST3: 'M0.1',        // Bit at M20.0
-      TEST4: 'M0.2',        // Bit at M20.0
-};
-
-conn.initiateConnection({ port: 102, host: '192.168.0.103', rack: 0, slot: 2, debug: false }, connected); // slot 2 for 300/400, slot 1 for 1200/1500, change debug to true to get more info
-// conn.initiateConnection({port: 102, host: '192.168.0.2', localTSAP: 0x0100, remoteTSAP: 0x0200, timeout: 8000, doNotOptimize: true}, connected);
-// local and remote TSAP can also be directly specified instead. The timeout option specifies the TCP timeout.
-
-function connected(err) {
+conn.initiateConnection({ port: 102, host: '192.168.0.103', rack: 0, slot: 2, debug: false }, (err) => {
   if (typeof(err) !== "undefined") {
     // We have an error. Maybe the PLC is not reachable.
     console.log(err);
     process.exit();
   }
-  conn.setTranslationCB(function(tag) { return variables[tag]; }); // This sets the "translation" to allow us to work with object names
-  conn.setTranslationCB(function(tag) { return variables[tag]; }); // This sets the "translation" to allow us to work with object names
-  conn.addItems('TEST4');
+  get();
   setInterval(() => {
-    conn.readAllItems(valuesReady);
+    if(firstLoad){
+      conn.readAllItems(valuesReady);
+    }
   }, 500);
+  
+});
+
+function get(){ //get data from firebase
+  const doc = db.collection('tags_s7');
+  doc.onSnapshot(docSnapshot => {//getting realtime changes
+      console.log(`Received doc snapshot:`);
+      const tag_list = docSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+      }));
+      let object = {};
+      if(tag_list.length === 0){
+        firstLoad = true;
+      }
+      tag_list.forEach(element => {
+          object[element.id] = element.value;
+      });
+      var changedTags = getChangedTags(object);
+      var labels = Object.keys(changedTags);
+      var values = Object.values(changedTags);
+      
+      if(labels.length > 0 && values.length > 0){
+        conn.addItems(labels);
+        conn.writeItems(labels, values, valuesWritten);
+      }
+      console.log('changedTags :>> ', changedTags);
+
+  }, err => {
+    console.log(`Encountered error: ${err}`);
+  });
 }
 
-function valuesReady(anythingBad, values) {
-  if (anythingBad) { console.log("SOMETHING WENT WRONG READING VALUES!!!!"); }
+function post(values){
+  var batch = db.batch();
+  Object.keys(values).forEach((el, i) => {
+    const doc = {
+      address: el,
+      value: values[el]
+    }
+    batch.set(db.collection('tags_s7').doc(el), doc);
+  });
+
+  batch.commit().then(function () {
+     console.log('batch ok');
+  });
+}
+
+function getChangedTags(values){
   var tagsOld = JSON.parse(JSON.stringify(tags));
   tags = values;
   var changedTags = {};
@@ -39,10 +78,15 @@ function valuesReady(anythingBad, values) {
     if(!(key in tagsOld) || value !== tagsOld[key]){
       changedTags[key] = Object.values(tags)[i];
     }
-    
   }
-  if(Object.keys(changedTags).length > 0){
-    console.log('changedTags', changedTags);
+  return changedTags;
+}
+
+function valuesReady(anythingBad, values) {
+  if (anythingBad) { console.log("SOMETHING WENT WRONG READING VALUES!!!!"); }
+  var changedTags = getChangedTags(values);
+  if(firstLoad && Object.keys(changedTags).length > 0){
+    post(changedTags)
   }
   // conn.readAllItems(valuesReady);
   // doneReading = true;
@@ -52,6 +96,7 @@ function valuesReady(anythingBad, values) {
 function valuesWritten(anythingBad) {
   if (anythingBad) { console.log("SOMETHING WENT WRONG WRITING VALUES!!!!"); }
   console.log("Done writing.");
-  doneWriting = true;
-  if (doneReading) { process.exit(); }
+  firstLoad = true;
+  // doneWriting = true;
+  // if (doneReading) { process.exit(); }
 }
